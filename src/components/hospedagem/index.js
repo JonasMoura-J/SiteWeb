d5d1c53456092ed876df034e60292377619f0a09
Paulo Eduardo Lucas Ferreira <dudu.magnificat@gmail.com>
2023-11-28 00:18:45 -0300

    Updating

--- src/components/hospedagem/index.js
+++ src/components/hospedagem/index.js
@@ -1,15 +1,16 @@
-var iptEmail = document.querySelector('input#cemail');
-var iptAssunto = document.querySelector('input#cassunto');
-var iptWhatsapp = document.querySelector('input#cwhatsapp');
-var iptMensagem = document.querySelector('input#cmensagem');
+var locaisDeHospedagem = [
+    { nome: 'Hotel Willisau', lat: -22.442669, lon: -42.9825237 },
+    { nome: 'Athos Hotel', lat: -22.4343485, lon: -42.9751807 },
+    { nome: 'Intercity Teresópolis', lat: -22.4170021, lon: -42.9748386 }
+];
 
-function alerta() {
+var map = L.map('map').setView([ -22.4343485, -42.9751807], 13);
 
-    alert('Guardamos suas informações logo te enviaremos notícias!');
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
 
-    localStorage.setItem("@TERE:email", iptEmail.value);
-    localStorage.setItem("@TERE:assunto", iptAssunto.value);
-    localStorage.setItem("@TERE:whatsapp", iptWhatsapp.value);
-    localStorage.setItem("@TERE:mensagem", iptMensagem.value);
-
-}
+locaisDeHospedagem.forEach(function(local) {
+    var marker = L.marker([local.lat, local.lon]).addTo(map);
+    marker.bindPopup(`<b>${local.nome}</b><br>Coordenadas: ${local.lat}, ${local.lon}`).openPopup();
+});
